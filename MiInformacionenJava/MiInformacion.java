b509870447bca5cee7e2cb05a23aa8a3de985505
mapaguerrero-cmyk <mapa.guerrero@gmail.com>
2026-06-02 15:27:03 -0400

    Mi primer commit con mis asignaciones Java en este repositorio

---
+++ MiInformacionenJava/MiInformacion.java
@@ -0,0 +1,13 @@
+public class MiInformacion {
+    public static void main(String[] args) {
+        String nombre = "Paulina Guerrero";
+        int edad = 60;
+        String ciudad = "Santiago de Chile";
+        String hobbies = "programar, estudiar, leer y cocinar";
+
+        System.out.println("Hola, mi nombre es " + nombre + ".");
+        System.out.println("Tengo " + edad + " años.");
+        System.out.println("Vivo en " + ciudad + ".");
+        System.out.println("Mis hobbies son: " + hobbies + ".");
+    }
+}
