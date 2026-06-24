3464d05e8d2377f3b336231197c8bde2716c950a
María Paulina Guerrero Vilches <mapa.guerrero@gmail.com>
2026-06-23 21:57:18 -0400

    (Core) insert de registros

---
+++ canciones/src/main/CorecancionesInsert.sql
@@ -0,0 +1,9 @@
+INSERT INTO canciones (titulo, artista, album, genero, idioma, fecha_creacion, fecha_actualizacion)VALUES 
+('La Bicicleta', 'Carlos Vives', 'La Bicicleta', 'Pop Latino', 'Español', CURDATE(), CURDATE()),
+('Tusa', 'Karol G', 'Tusa', 'Reggaeton', 'Español', CURDATE(), CURDATE());
+
+INSERT INTO canciones (titulo, artista, album, genero, idioma, fecha_creacion, fecha_actualizacion)
+VALUES 
+('El Baile de los Que Sobran', 'Los Prisioneros', 'Corazones', 'Rock', 'Español', CURDATE(), CURDATE()),
+('La Torre de Babel', 'Los Tres', 'Fome', 'Rock Alternativo', 'Español', CURDATE(), CURDATE()),
+('El Río', 'Javiera Parra', 'Invencible', 'Pop Rock', 'Español', CURDATE(), CURDATE());
